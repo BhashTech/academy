480867474cb428eaa888d8e34879e0f1431cf43e
Subhash <findtopfree@gmail.com>
2022-04-02 05:14:52 -0700

    Added N4 initial data

---
+++ src/Model/Card.cs
@@ -0,0 +1,30 @@
+﻿namespace Academy.Model
+{
+    public class Card
+    {
+        public Card(int paramId, string title, string flipTitle,
+                         string firstDesc, string secondDesc, string imagePath)
+        {
+            Id = paramId.ToString();
+            Title = title;
+            FlipTitle = flipTitle;
+            FlipFirstDesc = firstDesc;
+            FlipSecondDesc = secondDesc;
+            ImagePath = imagePath;
+        }
+
+        public Card()
+        {
+
+        }
+        public string Id { get; set; }
+        public string? Title { get; set; }
+        public string? FlipTitle { get; set; }
+        public string? FlipFirstDesc { get; set; }
+        public string? FlipSecondDesc { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Tags { get; set; }
+        public string? Levels { get; set; }
+    }
+
+}
---
+++ src/Services/JsonFileCardService.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Academy.Model;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Academy.Services
+{
+    public class JsonFileCardService
+    {
+        /*public async Task<IEnumerable<Card>> GetAllCards()
+        {
+            HttpClient httpClient;
+            return await httpClient.GetFromJsonAsync<Card[]>("sample-data/weather.json");
+        }*/
+
+    }
+}
